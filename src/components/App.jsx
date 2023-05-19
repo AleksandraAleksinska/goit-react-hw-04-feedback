72bf73bfbe2850cdbb1475c34d29eb9048979ff1
Aleksandra Aleksińska <aleksandra.aleksinska@gmail.com>
2023-05-19 23:18:55 +0200

    jsx update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,48 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Fragment } from "react";
+import { Component } from "react";
+
+// export const App = () => {
+//   return (
+//   <Fragment>
+//     <div>
+//     </div>
+//   </Fragment>
+//   );
+// };
+
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      good: 0,
+      neutral: 0,
+      bad: 0
+    };
+  }
+
+  countTotalFeedback = () => this.state.good + this.state.neutral + this.state.bad;
+  countPositiveFeedbackPercentage = () => Math.round(this.state.good / (this.countTotalFeedback()) * 100);
+  
+  
+
+  render () {
+    return (
+      <Fragment>
+        <section>Please leave feedback </section>
+        <button>Good</button>
+        <button>Neutral</button>
+        <button>Bad</button>
+        <span>Good: {this.state.good}</span>
+        <span>Neutral: {this.state.neutral}</span>
+        <span>Bad: {this.state.bad}</span>
+        <span>Total:{this.countTotalFeedback()}</span>
+        <span>Positive feedback: {this.countPositiveFeedbackPercentage()} %</span>
+      </Fragment> 
+      
+    )
+  }
+}
+
+export default App;
